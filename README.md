5083a16bed322801f5b330b671c313febac6d0cf
Eszter <92452454+zsuky2@users.noreply.github.com>
2022-09-03 12:12:12 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# webler_Git_vizsga_20220903
+Verziókezelés vizsga
+
+----------------------------------------------------------------------------------
+
+> 1. A main brachből hozzunk létre egy saját branchet, ide kell feltölteni a megoldást!
+> 2. Hozzunk létre egy könyv felvitelére szolgáló űrlapot a következők szerint:
+> Kérjük be a könyv ISBN számát (kötelező), címét (kötelező), szerzőjét (kötelező), kategóriáját (kötelező és tetszőleges kategóriákal, pld. dráma, életrajzi), árát (kötelező), ÁFA tartalom (0%, 10%, 27%) (kötelező), akciós -e vagy nem (alapértelmezetten nem akciós)
+> Mentés gombra kattintva írjuk ki a könyv minden adatát bruttó árral együtt!
+> A kinézethez Bootstrap használata javasolt!!!
+> Fontos, hogy mindenki a saját branchébe dolgozzon, melynek a neve a vezetéknék keresztnév kötöjellel, ékezet nélkül: Teszt-Elek (saját névvel)
---
+++ app.js
@@ -0,0 +1,99 @@
+var title = document.getElementById("title");
+var author = document.getElementById("author");
+var number = document.getElementById("number");
+var category = document.getElementById("category");
+var price = document.getElementById("price");
+
+var vatNullPercent = document.getElementById("vatNullPercent");
+var vatTenPercent = document.getElementById("vatTenPercent");
+var vatTwentySevenPercent = document.getElementById("vatTwentySevenPercent");
+
+var discounted = document.getElementById("discounted");
+var notDiscounted = document.getElementById("notDiscounted");
+
+var table = document.querySelector("table");
+var form = document.querySelector("form");
+
+
+
+form.addEventListener("submit", function (e) {
+
+    
+    e.preventDefault()
+
+    
+    addBook();
+})
+
+
+
+function addBook() {
+
+    var tr = document.createElement("tr");
+    var td1 = document.createElement("td");
+    var td2 = document.createElement("td");
+    var td3 = document.createElement("td");
+    var td4 = document.createElement("td");
+    var td5 = document.createElement("td");
+    var td6 = document.createElement("td");
+    var td7 = document.createElement("td");
+    var td8 = document.createElement("td");
+
+    td1.innerText = title.value;
+    td2.innerText = author.value;
+    td3.innerText = number.value;
+    td4.innerText = category.value;
+    td5.innerText = price.value+"Forint";
+    
+  
+
+   
+
+    if (title.value=="" || author.value =="" || number.value=="" || category.value=="" || price.value=="" || price.value<0){
+       
+        alert("Hiányosan töltötte ki vagy helytelen adatot adott meg.")
+        return
+    } else if (vatNullPercent.checked){
+
+        
+        td6.innerText = "0%";
+        var grossPrice = price.value*1;
+    } else if (vatTenPercent.checked){
+
+        td6.innerText = "10%";
+        var grossPrice = price.value*1.1;
+    } else {
+
+        td6.innerText = "27%";
+        var grossPrice = price.value*1.27;
+    }
+
+    if (discounted.checked){
+
+        td7.innerText = "Nem";
+    } else {
+
+        td7.innerText = "Igen";
+    }
+
+    td8.innerText=grossPrice+"Forint";
+
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
+    tr.appendChild(td5);
+    tr.appendChild(td6);
+    tr.appendChild(td7);
+    tr.appendChild(td8);
+    
+    table.appendChild(tr);
+
+    title.value = "";
+    author.value = "";
+    number.value = "";
+    category.value = "";
+    price.value = "";
+
+
+}
